5c27f9210103b6df3c7fd3abecdb13dc6c64a02e
Santosh <Santosh.axle@gmail.com>
2022-01-18 21:29:33 +0530

    project initial structure added

---
+++ exploreCalif/src/test/java/ec/springbt/explorecalif/ExploreCaliforniaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.springbt.explorecalif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreCaliforniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
